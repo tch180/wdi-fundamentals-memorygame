e1b1fcf45d5f3381a3027bedb9895dc17cf1f9ab
Tory <tch180>
2017-08-14 21:04:19 -0400

    created logic game

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-console.log ("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen" ;
-var cardTwo = "queen" ;
-var cardThree = "king" ;
-var cardFour = "king";
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardFour);
+var cardOne = cards [0]; 
+  cardsInPlay.push("queen");
+console.log("user flipped a" + cardOne);
+
+var cardTwo = cards [3];
+cardsInPlay.push("queen");
+console.log("user flipped a" + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay [0] === cardsInPlay [1]){
+  alert("you Found a match!");
+	} else {
+  alert("sorry try again");
+	}
+};
