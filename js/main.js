dc7b125c205bc726e6e7cc76896771f9ed1a8c4e
Tory Redner <toryredner@Torys-MacBook-Pro.local>
2017-08-22 13:42:46 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -24,16 +24,27 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-var flipCard = function(cardId){
-  cardsInPlay.push(cards[cardId].rank);
+var flipCard = function(){
+   var cardId = this.getAttribute('data-id');
+  this.setAttribute('src',cards[cardId].cardImage);
+   cardsInPlay.push(cards[cardId].rank);
   if (cardsInPlay.length === 2){
     checkForMatch();}
    console.log("user flipped " + cards[cardId].rank);
    console.log(cards[cardId].suit);
    console.log(cards[cardId].cardImage);
+  
   }
     
-
+var createBoard = function(){
+ for (var i = 0; i < cards.length; i++){
+  var cardElement = document.createElement('img');
+cardElement.setAttribute('src', "images/back.png");
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+}
+};
 
 var checkForMatch = function() {
  if (cardsInPlay[0] === cardsInPlay[1]){
@@ -45,4 +56,4 @@ var checkForMatch = function() {
     
 
 
-flipCard(0)
+createBoard();
